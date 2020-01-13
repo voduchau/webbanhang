0408f9d12128cd4cf465d69d055c0d396e1e564f
voduchau <voduchau01011997@gmail.com>
2020-01-13 13:26:15 +0700

    xoa route

--- banhang/routes/web.php
+++ banhang/routes/web.php
@@ -19,4 +19,3 @@ Route::get('/loaisanpham','PageController@getLoaisanpham');
 Route::get('/chitiet/{id}','PageController@getChitiet');
 Route::get('/lienhe','PageController@getLienhe');
 Route::get('/gioithieu','PageController@getGioithieu');
-Route::get('/gioithieu2','PageController@getGioithieu');
